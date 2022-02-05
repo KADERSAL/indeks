3fe9b878be9b942427ced73f6ede532faf4ef211
KEYDIR <99085581+KADERSAL@users.noreply.github.com>
2022-02-05 23:06:49 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,17 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        double boy, kilo;
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Boyunuzu (metre cinsinden) giriniz: ");
-        boy = input.nextDouble();
-
-        System.out.println("Kilonuzu giriniz: ");
-        kilo = input.nextDouble();
-
-        double indeks = kilo / (boy*boy);
-        System.out.println("Vücut kitle indeksiniz : " +indeks);
+        double armut = 2.14;
+        double elma = 3.67;
+        double domates = 1.11;
+        double muz = 0.95;
+        double patlican = 5.00;
+        Scanner input =new Scanner(System.in);
+        System.out.println("Kaç kg armut aldığınızı giriniz : ");
+        int a = input.nextInt();
+        System.out.println("Kaç kg elma aldığınızı giriniz : ");
+        int e = input.nextInt();
+        System.out.println("Kaç kg domates aldığınızı giriniz : ");
+        int d = input.nextInt();
+        System.out.println("Kaç kg muz aldığınızı giriniz : ");
+        int m = input.nextInt();
+        System.out.println("Kaç kg patlıcan aldığınızı giriniz : ");
+        int p = input.nextInt();
+        System.out.println("Armut kilo : " +a);
+        System.out.println("Elma kilo : " +e);
+        System.out.println("Domates kilo : " +d);
+        System.out.println("Muz kilo : " +m);
+        System.out.println("Patlıcan kilo : " +p);
+        double toplam = armut*a + elma*e + domates*d + muz*m + patlican*p;
+        System.out.println("Toplam tutar: " +toplam);
     }
 }
-
